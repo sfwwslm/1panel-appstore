3dca33e674ec4893e6b4d53849da2f36cdedbaef
sfwwslm <sfwwslm@gmail.com>
2026-01-28 11:28:06 +0800

    feat(install): 清理本地应用目录后再安装应用

--- install.sh
+++ install.sh
@@ -5,4 +5,6 @@ rm -rf /tmp/1panel-appstore
 
 git clone https://github.com/sfwwslm/1panel-appstore.git /tmp/1panel-appstore/
 
+rm -rf /opt/1panel/resource/apps/local/*
+
 cp -rf /tmp/1panel-appstore/apps/* /opt/1panel/resource/apps/local/
